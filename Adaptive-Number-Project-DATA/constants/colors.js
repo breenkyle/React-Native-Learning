5ae930d393a6ce875eec03b01b6705d7f23f289d
Kyle Breen <87739246+breenkyle@users.noreply.github.com>
2022-07-16 20:02:30 -0400

    Add files via upload
    Made changes to the Number Project app to make specific code changes fro iOS and Android ( did not include node.modules again due to size)

---
+++ Adaptive-Number-Project-DATA/constants/colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+  primary500: '#72063c',
+  primary600: '#640233',
+  primary700: '#4e0329',
+  primary800: '#3b021f',
+  accent500: '#ddb52f'
+};
+
+export default Colors;
+
+// You can make differernt files for Colors for the different OSs as well, just make sure to import them from just 'colors' and not the platform speficic files. 
